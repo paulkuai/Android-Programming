b60b33dd955ced014f988f21bd8d63de0d5703f8
paul <paulkuai0628@163.com>
2018-02-07 17:36:17 +0800

    完成第3章activity的周期，挑战练习1 禁止一题多答，挑战练习2 答完全部题后 给出百分比形式的评分

--- app/src/main/java/com/example/paul/myandroidparctice/GeoQuizActivity.java
+++ app/src/main/java/com/example/paul/myandroidparctice/GeoQuizActivity.java
@@ -1,14 +1,22 @@
 package com.example.paul.myandroidparctice;
 
+import android.os.MessageQueue;
+import android.security.keystore.KeyInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.DecimalFormat;
+
 public class GeoQuizActivity extends AppCompatActivity {
+
+    private static final String TAG = "GeoQuizActivity";
+    private static final String KEY_INDEX = "index";
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -30,6 +38,7 @@ public class GeoQuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: called");
         setContentView(R.layout.activity_geo_quiz);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
@@ -40,7 +49,7 @@ public class GeoQuizActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
-        updateQuestion();
+
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -49,7 +58,12 @@ public class GeoQuizActivity extends AppCompatActivity {
 //                Toast toast = Toast.makeText(GeoQuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT);
 //                toast.setGravity(Gravity.CENTER, 0, 0);
 //                toast.show();
-                checkAnswer(true);
+                if (mQuestionBank[mCurrentIndex].isAnswered()) {
+                    Toast.makeText(GeoQuizActivity.this, "This question is answered!", Toast.LENGTH_SHORT).show();
+                } else {
+                    checkAnswer(true);
+                }
+
             }
         });
 
@@ -66,19 +80,21 @@ public class GeoQuizActivity extends AppCompatActivity {
 //                toast.setGravity(Gravity.CENTER, 0, 0);
 //                toast.setDuration(Toast.LENGTH_LONG);
 //                toast.show();
+
+
                 checkAnswer(false);
+
             }
         });
 
-        mPrevButton = (ImageButton)findViewById(R.id.prev_button);
+        mPrevButton = (ImageButton) findViewById(R.id.prev_button);
         mPrevButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(mCurrentIndex!=0)
-                {
+                if (mCurrentIndex != 0) {
                     mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
-                }else{
-                    mCurrentIndex = mQuestionBank.length-1;
+                } else {
+                    mCurrentIndex = mQuestionBank.length - 1;
                 }
 
                 updateQuestion();
@@ -94,23 +110,106 @@ public class GeoQuizActivity extends AppCompatActivity {
             }
         });
 
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX);
+            mQuestionTextView.setText(mQuestionBank[mCurrentIndex].getTextResId());
+        }
+
+        updateQuestion();
     }
 
-    private void updateQuestion(){
-        int question = mQuestionBank[mCurrentIndex].getTextResId();
-        mQuestionTextView.setText(question);
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: called");
     }
 
-    private void checkAnswer(boolean userPressedTrue){
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: called");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstanceState: ");
+        outState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    private void updateQuestion() {
+        try {
+            int question = mQuestionBank[mCurrentIndex].getTextResId();
+            mQuestionTextView.setText(question);
+            if (mQuestionBank[mCurrentIndex].isAnswered()) {
+                //Toast.makeText(GeoQuizActivity.this, "You can just answer this question once!", Toast.LENGTH_SHORT).show();
+                mTrueButton.setEnabled(false);
+                mFalseButton.setEnabled(false);
+            } else {
+                mTrueButton.setEnabled(true);
+                mFalseButton.setEnabled(true);
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            Log.e(TAG, "Index was out of bounds.", e);
+        }
+
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
         boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
 
         int messageResId = 0;
-        if(userPressedTrue == answerIsTrue){
+        if (userPressedTrue == answerIsTrue) {
             messageResId = R.string.correct_toast;
-        }else{
+        } else {
             messageResId = R.string.incorrect_toast;
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+
+        mQuestionBank[mCurrentIndex].setAnswered(true);
+
+        updateQuestion();
+        checkisAllAnswered();
+
+    }
+
+    /**
+     * 每次答完题后检查是否全部答完，如全部答完则给出百分比结果
+     */
+    private void checkisAllAnswered() {
+        boolean isAllAnswered = true;
+        int correctCount = 0;
+        for (Question q : mQuestionBank) {
+            if (!q.isAnswered()) {
+                isAllAnswered = false;
+            }else{
+                if(q.isAnswerTrue()){
+                    correctCount+=1;
+                }
+            }
+        }
+        if (isAllAnswered) {
+            String msg = "共计"+mQuestionBank.length+"题，答对"+String.valueOf(correctCount)+"题，答题正确率为"+ new DecimalFormat("#.00").format((correctCount*1.00/mQuestionBank.length)*100)+"%";
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
--- app/src/main/java/com/example/paul/myandroidparctice/Question.java
+++ app/src/main/java/com/example/paul/myandroidparctice/Question.java
@@ -4,6 +4,7 @@ public class Question {
 
     private int mTextResId;
     private boolean mAnswerTrue;
+    private boolean mIsAnswered;
 
 
     public Question(int textResId, boolean answerTrue) {
@@ -25,5 +26,13 @@ public class Question {
 
     public void setAnswerTrue(boolean answerTrue) {
         mAnswerTrue = answerTrue;
+    }
+
+    public boolean isAnswered() {
+        return mIsAnswered;
+    }
+
+    public void setAnswered(boolean answered) {
+        mIsAnswered = answered;
     }
 }
